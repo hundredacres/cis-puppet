09daaebec71ce00bb1615ccc9aa819d4c18ba7a7
Matthew Schmitt <matthew.schmitt@riskiq.net>
2018-02-06 14:59:57 -0800

    Renaming old files to fix name space problems for Puppet 4

---
+++ manifests/el7/c1_5_1.pp
@@ -0,0 +1,8 @@
+# Class cis::el7::c1_5_1
+#
+# CIS Security Benchmark for RHEL7
+#
+
+class cis::el7::c1_5_1   {
+  include cis::linuxcontrols::c0011
+}
---
+++ manifests/el7/c2_1_18.pp
@@ -0,0 +1,8 @@
+# Class cis::el7::c2_1_18
+#
+# CIS Security Benchmark for RHEL7
+#
+
+class cis::el7::c2_1_18  {
+  include cis::linuxcontrols::c0033
+}
