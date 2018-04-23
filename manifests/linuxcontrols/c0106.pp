349b363111bec4c1209d19e3d6258f6f913c662b
Matthew Schmitt <matthew.schmitt@riskiq.net>
2018-04-23 11:12:32 -0700

    Adding script for remediating unlock time for failed logins

--- manifests/linuxcontrols/c0106.pp
+++ manifests/linuxcontrols/c0106.pp
@@ -1,6 +1,6 @@
 # Class cis::linuxcontrols::c0106
 #
-# Ensure prelink is disabled.
+# Ensure account lockout configured.
 #
 
 class cis::linuxcontrols::c0106 {
@@ -10,8 +10,17 @@ class cis::linuxcontrols::c0106 {
     group  => root,
     mode   => '0700',
   }
+  file {'/usr/local/sbin/f0025.sh':
+    source => 'puppet:///modules/cis/linuxcontrols/scripts/f0025.sh',
+    owner  => root,
+    group  => root,
+    mode   => '0700',
+  }
 
   if $::f0024 == 'fail' {
     warning("Node $::fqdn failed CIS v2.1.0 RHEL6 Control 5.3.2 (f0024)")
+  }
+  if $::f0025 == 'fail' {
+    warning("Node $::fqdn failed CIS v2.1.0 RHEL6 Control 5.3.2 (f0025)")
   }
 }
